0ae95812d136427972c6c0782c3d42012c630bee
rgojerabajas <140863090+rgojerabajas@users.noreply.github.com>
2024-09-29 21:43:14 +0000

    ADDED FIND MIDI AND FIND OCTAVE METHODS

--- Main.java
+++ Main.java
@@ -25,19 +25,48 @@ public class Main
   public static void main(String[] args)
   {
     /***** DECLARATION SECTION *****/
-    int semitonesFromA = 1;
-    double frequency; 
+    int semitonesFromA;
+    double frequency, octavesApart, midiNum;
+  
+
     
     /***** INITIALIZATION SECTION *****/
-    
+    semitonesFromA = 7;
     
     /***** INTRO SECTION *****/
 
     /***** PROCESSING SECTION *****/
+    
+    
     frequency = Math.pow(2, ((double) semitonesFromA / 12) ) * 440;
+    octavesApart = findOctave(frequency);
+    midiNum = findMidiNum(frequency);
+
+
     /***** OUTPUT SECTION *****/
-    System.out.println("Your frequency is "+frequency);
+    System.out.println("Your note is "+semitonesFromA+" notes away from A (440 Hz)");
+    System.out.println("Your frequency is "+frequency+" Hz");
+    System.out.println("Your frequency is "+octavesApart+" octaves away from A (440 Hz)");
+    System.out.printf("Your MIDI Num is %.0f%n",midiNum);
+    System.out.println(" _______________________________________\r\n" + //
+            "|  | | | |  |  | | | | | |  |  | | | |  |\r\n" + //
+            "|  | | | |  |  | | | | | |  |  | | | |  |\r\n" + //
+            "|  | | | |  |  | | | | | |  |  | | | |  |\r\n" + //
+            "|  |_| |_|  |  |_| |_| |_|  |  |_| |_|  |\r\n" + //
+            "|   |   |   |   |   |   |   |   |   |   |\r\n" + //
+            "|   |   |   |   |   | A |   |   |   |   |\r\n" + //
+            "|___|___|___|___|___|___|___|___|___|___|\r\n");
     
   }
   /***** STATIC METHODS *****/
+  
+  public static double findOctave(double frequency){
+    double octavesApart = (Math.log(frequency / 440))/(Math.log(2));
+    return octavesApart;
+  }
+  
+  public static double findMidiNum(double frequency){
+    double midiNum = 12*(Math.log(2)*(frequency / 440)) + 69;
+    return midiNum;
+  }
 } 
